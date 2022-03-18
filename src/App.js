20b60b48dab5cb188bcd05d49590efead796f00e
khosyk <hosy12@gmail.com>
2022-03-18 17:05:38 +0900

    feat($component):EventAnswer (save localstorage, get localstroage)
    EventAnswer component shows chart of total Data as if it is requested.
    
    currently there is no real data, so, it just shows graphics

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ function App() {
   const [isId, setIsId] = useState(null);
   const [vieSurvey, setViewSurvey] = useState(true);
   const [eventAsnwer, setEventAnswer] = useState(true);
-
+  const [data, setData] = useState(null);
   return (
     <div className="App">
       <GetUserId setViewSurvey={setViewSurvey} setIsId={setIsId} />
-      {vieSurvey ? <Survey setEventAnswer={setEventAnswer} isId={isId}/> : ''}
+      {vieSurvey ? <Survey setData={setData} setEventAnswer={setEventAnswer} isId={isId} /> : ''}
+      {eventAsnwer ? <EventAnswer /> : ''}
     </div>
   );
 }
 
-function GetUserId(setViewSurvey, setIsId) {
+function GetUserId({ setViewSurvey, setIsId }) {
 
   const [input, setInput] = useState({
     userId: null,
   })
-
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput({
+      userId: value
+    })
+  }
 
   const requestId = async () => {
     await axios.request(input.userId);
@@ -28,7 +34,6 @@ function GetUserId(setViewSurvey, setIsId) {
   }
 
   const showSurvey = (e) => {
-    e.preventDefault();
     if (input.userId !== '') {
       requestId();
       setIsId(input.userId);
@@ -37,7 +42,7 @@ function GetUserId(setViewSurvey, setIsId) {
       alert('아이디를 입력해주세요.')
     }
   }
-  
+
 
   return (
     <div>
@@ -46,7 +51,7 @@ function GetUserId(setViewSurvey, setIsId) {
         className='user_id'
         type='text'
         maxLength={12}
-        onInput={setInput}
+        onInput={(e) => onInput(e)}
         placeholder='아이디를 입력해주세요.'></input>
       <input
         className='user_id_btn'
@@ -58,8 +63,7 @@ function GetUserId(setViewSurvey, setIsId) {
   )
 }
 
-function Survey({setEventAnswer}, isId) {
-  const [data, setData] = useState(null);
+function Survey({ setEventAnswer, isId, setData }) {
   const [surveyDate, setSurveyDate] = useState([]);
   const [multi, setMulti] = useState([]);
   const [answers, setAnswers] = useState({
@@ -69,9 +73,8 @@ function Survey({setEventAnswer}, isId) {
     "4": [],
     "5": null,
   })
-  console.log(surveyDate[0]);
 
-  var now = new Date(); 
+  var now = new Date();
   var time = {
     year: now.getFullYear(),
     month: now.getMonth() + 1,
@@ -92,31 +95,31 @@ function Survey({setEventAnswer}, isId) {
       getData()
       getTime()
     };
-  },[answers, multi])
+  }, [answers, multi])
 
   const getTime = () => {
-  const {year, month, date, hours, minutes} = time;
-  const startTime = `${year}/${month}/${date} ${hours}:${minutes}`;
-  setSurveyDate([...surveyDate,startTime])
+    const { year, month, date, hours, minutes } = time;
+    const startTime = `${year}/${month}/${date} ${hours}:${minutes}`;
+    setSurveyDate([...surveyDate, startTime])
   }
 
   const handleCheck = (checked, id) => {
-    if(multi.includes('neither')) {
+    if (multi.includes('neither')) {
       if (checked) {
         setMulti([]);
         setMulti([id]);
-      } 
-    else {
-      setMulti(multi.filter((e) => e !== id))
-    }
-    }else{
+      }
+      else {
+        setMulti(multi.filter((e) => e !== id))
+      }
+    } else {
       if (checked) {
-        
+
         setMulti([...multi, id]);
-      } 
-    else {
-      setMulti(multi.filter((e) => e !== id))
-    }
+      }
+      else {
+        setMulti(multi.filter((e) => e !== id))
+      }
     }
   }
 
@@ -136,13 +139,13 @@ function Survey({setEventAnswer}, isId) {
   }
 
   const handleSend = () => {
-    if(answers[1] && answers[2] && answers[3] && answers[4] !== [] && answers[5]){
+    if (answers[1] && answers[2] && answers[3] && answers[4] !== [] && answers[5]) {
       setEventAnswer(true);
-      setAnswers({...answers,"4" : multi});
+      setAnswers({ ...answers, "4": multi });
       sendData();
       localStorage.setItem('user_id', JSON.stringify(isId))
       localStorage.setItem('value', JSON.stringify(answers))
-    }else{
+    } else {
       alert('모든 질문에 답변 하셔야 합니다.');
       return;
     }
@@ -174,7 +177,7 @@ function Survey({setEventAnswer}, isId) {
           <tr className='updated_datetime'>
             <td>
               업데이트 시간
-              {surveyDate ? surveyDate[surveyDate.length -1] : ''}
+              {surveyDate ? surveyDate[surveyDate.length - 1] : ''}
             </td>
           </tr>
           <tr className="block1">
@@ -248,7 +251,7 @@ function Survey({setEventAnswer}, isId) {
             <td>
               <input checked={multi.includes('coop') ? true : false} type="checkbox" id="coop" name="commu" onChange={(e) => handleCheck(e.target.checked, e.target.id)} />
               <label htmlFor="commu">협력적이다.</label>
-              <input checked={multi.includes('nar') ? true : false}  type="checkbox" id="nar" name="commu" onChange={(e) => handleCheck(e.target.checked, e.target.id)} />
+              <input checked={multi.includes('nar') ? true : false} type="checkbox" id="nar" name="commu" onChange={(e) => handleCheck(e.target.checked, e.target.id)} />
               <label htmlFor="commu">의사소통을 잘한다.</label>
               <input type="checkbox" id="neither" name="commu" onChange={(e) => handleResetCheck(e.target.checked, e.id)} />
               <label htmlFor="commu">둘 다 아니다.</label>
@@ -283,6 +286,51 @@ function Survey({setEventAnswer}, isId) {
           </tr>
         </tbody>
       </table>
+    </>
+  )
+}
+
+function EventAnswer({data:totalData}) {
+  const user_id = localStorage.getItem('user_id');
+  const data = localStorage.getItem('value');
+  {/*
+  만약 진짜 데이터가 있었다면 숫자 부분에는 다시 axios.get 을 이용한 토탈 데이터의 정보가 들어갑니다.
+  */}
+  return (
+    <>
+      <button onClick={() => {
+        console.log(user_id);
+        console.log(data);
+      }}>e</button>
+      <h2>설문조사 통계 with.{user_id}</h2>
+      <div class='chart'>
+        <div>
+          <dl>
+          <dt>서비스 지향성</dt>
+          <dd><strong>7.9%</strong> <em>25</em></dd>
+          </dl>
+        </div>
+        <div>
+          <dl>업무 전문성
+          <dd><strong>13%</strong> <em>41</em></dd>
+          </dl>
+        </div>
+        <div>
+          <dl>능동적 도전성
+          <dd><strong>20.9%</strong> <em>66</em></dd>
+          </dl>
+        </div>
+        <div>
+          <dl>협업과 커뮤니케이션
+          <dd><strong>35.1%</strong> <em>111</em></dd>
+          </dl>
+        </div>
+        <div>
+          <dl>긍정 에너지
+          <dd><strong>23.1%</strong> <em>73</em></dd>
+          </dl>
+        </div>
+      </div>
     </>
   )
 }
