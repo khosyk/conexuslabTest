967e0f16f0495b0a576b33c07ef37783d13f14d9
khosyk <hosy12@gmail.com>
2022-03-18 16:23:20 +0900

    feat($browser): App, GetUserId, Survey components (contents/ functions)
    Created new components
    - App component keeps states to visulaize Survey
    - GetUserId component is for request user_id & shows Survey component
    - Survey component display questions & inputs, it collect all answers in 'answers' state
    - multi answers question(which is answers[4]) has 'multi' state to update 'answers'.

---
+++ README.md
@@ -0,0 +1,24 @@
+
+--- 전체 요구사항 ---
+
+0. indent depth < 2
+0. 함수 길이 50라인 미만
+0. 함수가 한번에 한가지 일만
+0. README 정리대로 commit 할 것
+0. [AngularJS Commit Message Conventions]로 commit log를 남긴다.
+
+--- 프로젝트 욕사항 ---
+
+0. 미동작 URL을 동작하는 것처럼 꾸며서 로직 생성
+0. 모바일과 태블릿 사이즈를 고려하여 스타일을 입힘 
+ - 480 768 1080 1536 1920 (width)
+
+1. GET, json 데이터를 응답 받은 것처럼 설문 조사 생성
+
+2. POST, 설문 조사 데이터 전송 
+
+3. 전송 완료 후 전달 받은 통계 데이터 화면 구성
+
+4. 스타일 수정
+  - Event Header Image
+  - Event BLocks (Title, Item List)
---
+++ src/App.js
@@ -0,0 +1,272 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [isId, setIsId] = useState(null);
+  const [vieSurvey, setViewSurvey] = useState(true);
+  const [eventAsnwer, setEventAnswer] = useState(true);
+
+  return (
+    <div className="App">
+      <GetUserId setViewSurvey={setViewSurvey} setIsId={setIsId} />
+      {vieSurvey ? <Survey setEventAnswer={setEventAnswer} isId={isId}/> : ''}
+    </div>
+  );
+}
+
+function GetUserId(setViewSurvey, setIsId) {
+
+  const [input, setInput] = useState({
+    userId: null,
+  })
+
+
+  const requestId = async () => {
+    await axios.request(input.userId);
+    setViewSurvey(true);
+  }
+
+  const showSurvey = (e) => {
+    e.preventDefault();
+    if (input.userId !== '') {
+      requestId();
+      setIsId(input.userId);
+      document.querySelector('table').style.display = 'block';
+    } else {
+      alert('아이디를 입력해주세요.')
+    }
+  }
+  
+
+  return (
+    <div>
+      <span>ID를 입력 하시면 설문조사를 진행하실수 있습니다.</span>
+      <input
+        className='user_id'
+        type='text'
+        maxLength={12}
+        onInput={setInput}
+        placeholder='아이디를 입력해주세요.'></input>
+      <input
+        className='user_id_btn'
+        type='submit'
+        onClick={() => showSurvey()}
+      >
+      </input>
+    </div>
+  )
+}
+
+function Survey({setEventAnswer}, isId) {
+  const [data, setData] = useState(null);
+  const [surveyDate, setSurveyDate] = useState([]);
+  const [multi, setMulti] = useState([]);
+  const [answers, setAnswers] = useState({
+    "1": null,
+    "2": null,
+    "3": null,
+    "4": [],
+    "5": null,
+  })
+  console.log(surveyDate[0]);
+
+  var now = new Date(); 
+  var time = {
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    date: now.getDate(),
+    hours: now.getHours(),
+    minutes: now.getMinutes(),
+  };
+
+  const getData = async () => {
+    await axios.get('http://event.conexuslab.co.kr/survey/1')
+      .then((data) => setData(data.json()))
+  }
+
+  useEffect(() => {
+    getTime()
+    getData();
+    return () => {
+      getData()
+      getTime()
+    };
+  },[answers, multi])
+
+  const getTime = () => {
+  const {year, month, date, hours, minutes} = time;
+  const startTime = `${year}/${month}/${date} ${hours}:${minutes}`;
+  setSurveyDate([...surveyDate,startTime])
+  }
+
+  const handleCheck = (checked, id) => {
+    if(multi.includes('neither')) {
+      if (checked) {
+        setMulti([]);
+        setMulti([id]);
+      } 
+    else {
+      setMulti(multi.filter((e) => e !== id))
+    }
+    }else{
+      if (checked) {
+        
+        setMulti([...multi, id]);
+      } 
+    else {
+      setMulti(multi.filter((e) => e !== id))
+    }
+    }
+  }
+
+  const handleResetCheck = (checked, id) => {
+    if (checked) {
+      setMulti(['neither']);
+    } else {
+      setMulti([]);
+    }
+  }
+
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr className='input_id'>
+            <td>
+              ID. 012
+            </td>
+          </tr>
+          <tr className='title'>
+            <td>
+              커넥서스랩 설문조사
+            </td>
+          </tr>
+          <tr className='input_header'>
+            <td>
+              <img src='#' alt='#'></img>
+            </td>
+          </tr>
+          <tr className='created_datetime'>
+            <td>
+              {surveyDate ? surveyDate[0] : ''}
+            </td>
+          </tr>
+          <tr className='updated_datetime'>
+            <td>
+              업데이트 시간
+              {surveyDate ? surveyDate[surveyDate.length -1] : ''}
+            </td>
+          </tr>
+          <tr className="block1">
+            <th>서비스지향성</th>
+            <td>
+              <div>
+                <input
+                  type="radio" id="low" name="degree1" value="low" onClick={() => setAnswers({ ...answers, "1": [0] })}
+                />
+                <label htmlFor="degree1">나쁨</label>
+              </div>
+              <div>
+                <input type="radio" id="mid" name="degree1" value="mid"
+                  onClick={() => setAnswers({ ...answers, "1": [1] })}
+                />
+                <label htmlFor="degree1">보통</label>
+              </div>
+              <div>
+                <input type="radio" id="high" name="degree1" value="high" onClick={() => setAnswers({ ...answers, "1": [2] })}
+                />
+                <label htmlFor="degree1">좋음</label>
+              </div>
+            </td>
+          </tr>
+          <tr className="block2">
+            <th>업무 전문성</th>
+            <td>
+              <div>
+                <input
+                  type="radio" id="low" name="degree2" value="low" onClick={() => setAnswers({ ...answers, "2": [0] })}
+                />
+                <label htmlFor="degree2">나쁨</label>
+              </div>
+              <div>
+                <input type="radio" id="mid" name="degree2" value="mid"
+                  onClick={() => setAnswers({ ...answers, "2": [1] })}
+                />
+                <label htmlFor="degree2">보통</label>
+              </div>
+              <div>
+                <input type="radio" id="high" name="degree2" value="high" onClick={() => setAnswers({ ...answers, "2": [2] })}
+                />
+                <label htmlFor="degree2">좋음</label>
+              </div>
+            </td>
+          </tr>
+          <tr className="block3">
+            <th>능동적 도전성</th>
+            <td>
+              <div>
+                <input
+                  type="radio" id="low" name="degree3" value="low" onClick={() => setAnswers({ ...answers, "3": [0] })}
+                />
+                <label htmlFor="degree3">나쁨</label>
+              </div>
+              <div>
+                <input type="radio" id="mid" name="degree3" value="mid"
+                  onClick={() => setAnswers({ ...answers, "3": [1] })}
+                />
+                <label htmlFor="degree3">보통</label>
+              </div>
+              <div>
+                <input type="radio" id="high" name="degree3" value="high" onClick={() => setAnswers({ ...answers, "3": [2] })}
+                />
+                <label htmlFor="degree3">좋음</label>
+              </div>
+            </td>
+          </tr>
+          <tr className="block4">
+            <th>협업과 커뮤니케이션</th>
+            <td>
+              <input checked={multi.includes('coop') ? true : false} type="checkbox" id="coop" name="commu" onChange={(e) => handleCheck(e.target.checked, e.target.id)} />
+              <label htmlFor="commu">협력적이다.</label>
+              <input checked={multi.includes('nar') ? true : false}  type="checkbox" id="nar" name="commu" onChange={(e) => handleCheck(e.target.checked, e.target.id)} />
+              <label htmlFor="commu">의사소통을 잘한다.</label>
+              <input type="checkbox" id="neither" name="commu" onChange={(e) => handleResetCheck(e.target.checked, e.id)} />
+              <label htmlFor="commu">둘 다 아니다.</label>
+            </td>
+          </tr>
+          <tr className="block5">
+            <th>긍정 에너지</th>
+            <td>
+              <div>
+                <input
+                  type="radio" id="low" name="degree5" value="low" onClick={() => setAnswers({ ...answers, "5": [0] })}
+                />
+                <label htmlFor="degree5">나쁨</label>
+              </div>
+              <div>
+                <input type="radio" id="mid" name="degree5" value="mid"
+                  onClick={() => setAnswers({ ...answers, "5": [1] })}
+                />
+                <label htmlFor="degree5">보통</label>
+              </div>
+              <div>
+                <input type="radio" id="high" name="degree5" value="high" onClick={() => setAnswers({ ...answers, "5": [2] })}
+                />
+                <label htmlFor="degree5">좋음</label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button onClick={() => ''}>제출</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App;
